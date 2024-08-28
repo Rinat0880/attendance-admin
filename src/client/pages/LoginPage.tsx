b9dd321289836411f4351ccb0efb1ed67990f6e0
Mambetlepesov Rinat <rinatmambetlepesov@gmail.com>
2024-08-28 10:24:16 +0500

    Initial comm

--- src/client/pages/LoginPage.tsx
+++ src/client/pages/LoginPage.tsx
@@ -182,22 +182,6 @@ const LoginPage: React.FC<LoginPageProps> = ({ onLoginSuccess }) => {
           >
             Sign in
           </Button>
-          <Link
-  component={RouterLink}
-  to="/qrscanner"
-  variant="body2"
-  sx={{ mt: 2, display: 'block' }} // display: 'block' переводит ссылку на новую строку
->
-  QRcode-Reader
-</Link>
-<Link
-  component={RouterLink}
-  to="/bigTable"
-  variant="body2"
-  sx={{ mt: 2, display: 'block' }} // display: 'block' переводит ссылку на новую строку
->
-  AttendanceTable
-</Link>
         </Box>
       </Box>
     </Container>
